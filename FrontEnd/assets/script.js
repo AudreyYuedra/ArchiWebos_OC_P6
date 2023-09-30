53db42248d4623e3b1ed45bd595c46aa50cfea40
Audrey Luquet <audrey.luquet@tutanota.com>
2023-09-30 16:35:08 +0200

    affichage works (non effectif)

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -1,10 +1,12 @@
 /********** CONSTANTES **********/
 //récupération DOM
+const portfolio = document.getElementById("portfolio");
 const gallery = document.getElementsByClassName("gallery");
 
 //requête API ressource works
-const works = await fetch("http://localhost:5678/api/works");
-const reponse = works.json();
+const works = fetch("http://localhost:5678/api/works").then(function() {
+    const reponse = works.json();
+});
 console.log(works);
 
 /********** VARIABLES **********/
@@ -12,3 +14,18 @@ console.log(works);
 
 
 /********** FONCTIONS **********/
+function afficherWorks(i) {
+    //création éléments
+    let work = document.createElement("figure");
+    let img = document.createElement("img");
+    let title = document.createElement("figcaption");
+    //position éléments
+    gallery.appendChild(figure);
+    figure.appendChild(img, figcaption);
+    //lien CSS
+    img.classList.add("gallery img");
+}
+
+for (i = 0; i > works.length; i++) {
+    afficherWorks(i)
+}
