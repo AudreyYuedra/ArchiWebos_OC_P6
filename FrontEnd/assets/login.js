24ed60e429fb439463cf49ef3056c280c98376e5
Audrey Luquet <audrey.luquet@tutanota.com>
2023-10-01 14:15:39 +0200

    création login.js

---
+++ FrontEnd/assets/login.js
@@ -0,0 +1,16 @@
+/********** CONSTANTES **********/
+//récupération DOM
+const email = document.getElementById("mail");
+const motDePasse = document.getElementById("mdp");
+const btnEnvoie = document.getElementById("btn-login");
+
+
+/********** VARIABLES **********/
+
+
+
+/********** FONCTIONS **********/
+
+
+
+/********** AUTRES ***********/
