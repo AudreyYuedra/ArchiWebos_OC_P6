1a03e7d8dca53534383df8c8ca5749ec935a056f
Audrey Luquet <audrey.luquet@tutanota.com>
2023-10-01 15:27:42 +0200

    vérification login

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,7 +1,8 @@
 /********** CONSTANTES **********/
 //récupération DOM
-const email = document.getElementById("mail");
-const motDePasse = document.getElementById("mdp");
+const form = document.querySelector("form");
+const baliseMail = document.getElementById("mail");
+const baliseMdp = document.getElementById("mdp");
 const btnEnvoie = document.getElementById("btn-login");
 
 
@@ -10,7 +11,45 @@ const btnEnvoie = document.getElementById("btn-login");
 
 
 /********** FONCTIONS **********/
+function verifierChamp(balise) {
+    if(balise.value === ""){
+        balise.classList.add("error");
+    } else {
+        balise.classList.remove("error");
+    }
+};
 
-
+function verifierEmail(baliseMail) {
+    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+");
+    if (emailRegExp.test(baliseMail.value)) {
+        console.log("L'email est valide !");
+        baliseMail.classList.remove("error");
+    } else {
+        console.log("L'email est erroné !");
+        baliseMail.classList.add("error");
+    }
+};
+function verifierMdp(baliseMdp) {
+    let mdpRegExp = new RegExp("[a-z0-9._-]+$");
+    if (mdpRegExp.test(baliseMdp)) {
+        console.log("Le mot de passe est valide !");
+        baliseMail.classList.remove("error");
+    } else {
+        console.log("Le mot de passe est erroné !");
+        baliseMail.classList.add("error");
+    }
+}
 
 /********** AUTRES ***********/
+//connexion login
+form.addEventListener("submit", (event) => {
+    //empêcher comportement par défaut
+    event.preventDefault();
+
+    verifierChamp();
+    verifierEmail();
+    verifierMdp();
+
+    //redirection si login OK
+    
+});
