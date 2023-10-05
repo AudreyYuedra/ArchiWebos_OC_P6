2adfbae5c08bff5016ce94744ac58500a4c2ac24
Audrey Luquet <audrey.luquet@tutanota.com>
2023-10-05 18:34:07 +0200

    mise en place fonction filtre (non effectif)

--- FrontEnd/assets/script.js
+++ FrontEnd/assets/script.js
@@ -38,6 +38,7 @@ function afficherWorks(works) {
     })
 }
 
+//requête API ressource categories
 function fetchCategories(){
     return fetch("http://localhost:5678/api/categories")
         .then(response => response.json()) //mettre paraenthèse pour contenu
@@ -49,7 +50,7 @@ function fetchCategories(){
 function afficherCategories(categories) {
     console.log("catégories", categories);
     //création élements
-    const filterAll = document.createElement("tous");
+    const filterAll = document.createElement("button");
     //position élément
     filters.appendChild(filterAll);
     //nom
@@ -58,7 +59,7 @@ function afficherCategories(categories) {
     filterAll.classList.add("btnFilter");
     //création éléments via boucle
     categories.forEach(category => {
-        const btnFilter = document.createElement("btnFilter");
+        const btnFilter = document.createElement("button");
         //assignements
         btnFilter.innerText = category.name
         //position éléments
@@ -68,6 +69,64 @@ function afficherCategories(categories) {
     })
 }
 
+// affichage filtre
+function filterAll(categories) {
+    //affichage de base
+    const btnAll = document.querySelector("button");
+    btnAll.classList.add(".filter_selected");
+    //SI click dessus
+    if ("click") {
+        //affiche tous les travaux
+        categories.filter((category) => category.id);
+        //lien CSS
+        btn.classList.remove("filter_selected");
+        btnAll.classList.add(".filter_selected");
+
+        console.log("Vous avez cliqué sur le filtre \"Tous\".");
+    }
+}
+
+function filterObject(categories) {
+    //affichage de base
+    const btnObject = btnFilter[0];
+    //SI click dessus
+    if ("click") {
+        categories.filter((category) => category.id[1]);
+        //lien CSS
+        btn.classList.remove("filter_selected");
+        btnObject.classList.add("filter_selected");
+
+        console.log("Vous avez cliqué sur le filtre \"Objets\".");
+    }
+}
+
+function filterAppart(categories) {
+    //affichage de base
+    const btnAppart = btnFilter[1];
+    //SI click dessus
+    if ("click") {
+        categories.filter((category) => category.id[2]);
+        //lien CSS
+        btn.classList.remove("filter_selected");
+        btnAppart.classList.add("filter_selected");
+
+        console.log("Vous avez cliqué sur le filtre \"Appartements\".");
+    }
+}
+
+function filterHotelResto(categories) {
+    //affichage de base
+    const btnHotelResto = btnFilter[2];
+    //SI click dessus
+    if ("click") {
+        categories.filter((category) => category.id[3]);
+        //lien CSS
+        btn.classList.remove("filter_selected");
+        btnHotelresto.classList.add("filter_selected");
+
+        console.log("Vous avez cliqué sur le filtre \"Hôtels & Restaurants\".");
+    }
+}
 
 /********** AUTRES **********/
  document.addEventListener("DOMContentLoaded", () => {
@@ -79,4 +138,11 @@ function afficherCategories(categories) {
         .then(categories => {
             afficherCategories(categories);
         });
+ });
+
+ button.addEventListener("click", () => {
+    filterAll();
+    filterObject();
+    filterAppart();
+    filterHotelResto();
  });
