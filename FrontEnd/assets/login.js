f483084e562083262a6e4d0b3ca7891f4eda0522
Audrey Luquet <audrey.luquet@tutanota.com>
2023-10-12 09:41:57 +0200

    suite login (semi-effectif)

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,20 +1,50 @@
 /********** CONSTANTES **********/
 //récupération DOM
 const main = document.querySelector("main");
-const baliseMail = document.getElementById("mail");
+const form = document.querySelector("form");
+const baliseMail = document.getElementById("email");
 const baliseMdp = document.getElementById("mdp");
-const btnEnvoie = document.getElementById("btn_login");
 
-const mail = "sophie.bluel@test.tld";
-const mdp = "S0phie";
+const login = [
+    mail = "sophie.bluel@test.tld",
+    mdp = "S0phie"
+];
 
 
 /********** VARIABLES **********/
 
 
-
 /********** FONCTIONS **********/
-function verifierChamp(balise) {
+async function verifLogin (login) {
+    if (baliseMail === mail.value & baliseMdp === mdp.value) {
+        //changement de page
+        window.location.href = "./index.html";
+    } else {
+        //affiche message erreur
+        const txtError = document.createElement("txt_error");
+        main.appendChild(txtError);
+        txtError.innerHTML = "Erreur dans l'email ou/et le mot de passe.";
+        txtError.classList.add("txt_error");
+
+        console.log("Erreur de connexion avec l'email ou/et le mot de passe.");
+    }
+}
+
+    /********** AUTRES ***********/
+//connexion login
+form.addEventListener("submit", (event) => {
+    //empêcher comportement par défaut
+    event.preventDefault();
+
+    verifLogin();
+});
+
+
+    /*verifierChamp();
+    verifierEmail();
+    verifierMdp();*/
+
+    /*function verifierChamp(balise) {
     if(balise.value === ""){
         balise.classList.add("error");
     } else {
@@ -41,36 +71,4 @@ function verifierMdp(baliseMdp) {
         console.log("Le mot de passe est erroné !");
         baliseMail.classList.add("error");
     }
-}
-
-async function login(baliseMail, baliseMdp) {
-    btnEnvoie.innerText = "Se connecter";
-    btnEnvoie.classList.add("btn_login");
-
-    if (baliseMail === mail & baliseMdp === mdp) {
-        //changement de page
-        // ... (index.html)
-    } else {
-        //affiche erreur
-        const txtError = document.createElement("txt_error");
-        main.appendChild("txtError");
-        txtError.innerHTML = "Erreur d'identifiant ou de mot de passe.";
-        txtError.classList.add("txt_error");
-
-        console.log("Erreur de connexion.");
-    }
-}
-
-
-    /********** AUTRES ***********/
-//connexion login
-form.addEventListener("submit", (event) => {
-    //empêcher comportement par défaut
-    event.preventDefault();
-
-    verifierChamp();
-    verifierEmail();
-    verifierMdp();
-
-    login();
-});
+}*/
