33d1981b07d7c3f22cb69dd8e6825bfe35daddd1
Audrey Luquet <audrey.luquet@tutanota.com>
2023-10-12 13:16:09 +0200

    suite login try-catch (non effectif)

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -2,10 +2,9 @@
 //récupération DOM
 const main = document.querySelector("main");
 const form = document.querySelector("form");
-const baliseMail = document.getElementById("email");
-const baliseMdp = document.getElementById("mdp");
+const txtError = document.querySelector("txt_error");
 
-const login = [
+const loginValid = [
     mail = "sophie.bluel@test.tld",
     mdp = "S0phie"
 ];
@@ -15,18 +14,24 @@ const login = [
 
 
 /********** FONCTIONS **********/
-async function verifLogin (login) {
-    if (baliseMail === mail.value & baliseMdp === mdp.value) {
+async function verifLogin () {
+    const login = {
+        user: event.target.querySelector("[id=email]").value,
+        password: event.target.querySelector("[id=mdp]").value,
+    };
+    const chargeUtile = JSON.stringify(login);
+    
+    fetch ("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {"Content-type": "application.json"},
+        body: chargeUtile
+    });
+
+    if (login.user === mail.value & login.password === mdp.value) {
         //changement de page
         window.location.href = "./index.html";
     } else {
-        //affiche message erreur
-        const txtError = document.createElement("txt_error");
-        main.appendChild(txtError);
-        txtError.innerHTML = "Erreur dans l'email ou/et le mot de passe.";
-        txtError.classList.add("txt_error");
-
-        console.log("Erreur de connexion avec l'email ou/et le mot de passe.");
+        throw error;
     }
 }
 
@@ -36,7 +41,23 @@ form.addEventListener("submit", (event) => {
     //empêcher comportement par défaut
     event.preventDefault();
 
-    verifLogin();
+    try {
+        verifLogin();        
+    }
+    catch (error) {
+        if (login.user !== mail.value) {
+            txtError.innerHTML = "Erreur dans l'email.";
+            console.log("Erreur de connexion avec l'email.");
+        }
+        if (login.password !== mdp.value) {
+            txtError.innerHTML = "Erreur dans le mot de passe.";
+            console.log("Erreur de connexion avec le mot de passe.");
+        }
+        if (login.user ==="", login.password ==="") {
+            txtError.innerHTML = "L'un des champs est vide.";
+            console.log("L'un des champs est vide.");
+        }
+    }
 });
 
 
