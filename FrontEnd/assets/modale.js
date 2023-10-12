114960581efe77812aca058202ac61ff55268b8d
Audrey Luquet <audrey.luquet@tutanota.com>
2023-10-12 09:50:57 +0200

    création modale.js

---
+++ FrontEnd/assets/modale.js
@@ -0,0 +1,15 @@
+/********** CONSTANTES **********/
+const headband = document.querySelector(".headband");
+
+// login ok
+//token placé ici
+
+
+/********** VARIABLES **********/
+
+
+/********** FONCTIONS **********/
+function showHeadband () {}
+
+
+/********** AUTRES **********/
