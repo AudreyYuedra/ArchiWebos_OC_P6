b29a37ed1e655191f32273a67648fed290b91fa2
Audrey Luquet <audrey.luquet@tutanota.com>
2023-10-16 08:33:58 +0200

    fin login (effectif)

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -1,31 +1,22 @@
 /********** CONSTANTES **********/
 //récupération DOM
-const main = document.querySelector("main");
 const form = document.querySelector("form");
 const txtError = document.querySelector("txt_error");
-
-const loginValid = [
-    mail = "sophie.bluel@test.tld",
-    mdp = "S0phie"
-];
-
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
 
 /********** VARIABLES **********/
 
 
 /********** FONCTIONS **********/
-async function verifLogin () {
+/*async function verifLogin () {
     const login = {
         user: event.target.querySelector("[id=email]").value,
         password: event.target.querySelector("[id=mdp]").value,
     };
-    const chargeUtile = JSON.stringify(login);
     
-    fetch ("http://localhost:5678/api/users/login", {
-        method: "POST",
-        headers: {"Content-type": "application.json"},
-        body: chargeUtile
-    });
+    
+    
 
     if (login.user === mail.value & login.password === mdp.value) {
         //changement de page
@@ -33,32 +24,34 @@ async function verifLogin () {
     } else {
         throw error;
     }
-}
+}*/
 
     /********** AUTRES ***********/
 //connexion login
-form.addEventListener("submit", (event) => {
+form.addEventListener("submit", async (event) => {
     //empêcher comportement par défaut
     event.preventDefault();
 
-    try {
-        verifLogin();        
+    //if (email.value && password.value) {
+
+        fetch ("http://localhost:5678/api/users/login", {
+        method: "POST",
+        headers: {"Content-type": "application/json"},
+        body: JSON.stringify({
+            email: email.value,
+            password: password.value})
+        }).then(response => response.json())
+        .then (data => {
+            const token = data.token;
+            localStorage.setItem("Token", token);
+            if (token) {
+                window.location.replace("index.html");
+            } else {
+                const message = txtError;
+            }
+        })
     }
-    catch (error) {
-        if (login.user !== mail.value) {
-            txtError.innerHTML = "Erreur dans l'email.";
-            console.log("Erreur de connexion avec l'email.");
-        }
-        if (login.password !== mdp.value) {
-            txtError.innerHTML = "Erreur dans le mot de passe.";
-            console.log("Erreur de connexion avec le mot de passe.");
-        }
-        if (login.user ==="", login.password ==="") {
-            txtError.innerHTML = "L'un des champs est vide.";
-            console.log("L'un des champs est vide.");
-        }
-    }
-});
+);
 
 
     /*verifierChamp();
@@ -93,3 +86,22 @@ function verifierMdp(baliseMdp) {
         baliseMail.classList.add("error");
     }
 }*/
+
+
+/*    try {
+        //verifLogin();        
+    }
+    catch (error) {
+        if (login.user !== mail.value) {
+            txtError.innerHTML = "Erreur dans l'email.";
+            console.log("Erreur de connexion avec l'email.");
+        }
+        if (login.password !== mdp.value) {
+            txtError.innerHTML = "Erreur dans le mot de passe.";
+            console.log("Erreur de connexion avec le mot de passe.");
+        }
+        if (login.user ==="", login.password ==="") {
+            txtError.innerHTML = "L'un des champs est vide.";
+            console.log("L'un des champs est vide.");
+        }
+    }*/
