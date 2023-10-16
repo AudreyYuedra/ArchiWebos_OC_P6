aa44c98650ed2d07db1041019ce0aa57069bfe0d
Audrey Luquet <audrey.luquet@tutanota.com>
2023-10-16 09:41:43 +0200

    correction ajout vérif login

--- FrontEnd/assets/login.js
+++ FrontEnd/assets/login.js
@@ -56,8 +56,6 @@ form.addEventListener("submit", async (event) => {
         //changement de page
         if (token) {
             window.location.replace("index.html");
-        } else {
-            //txtError.innerText = "Il y a une erreur !";
         }
 
         //vérification email & password
@@ -66,70 +64,3 @@ form.addEventListener("submit", async (event) => {
         verifLogin();
     })
 });
-
-
-    /*verifierChamp();
-    verifierEmail();
-    verifierMdp();*/
-
-    /*function verifierChamp(balise) {
-    if(balise.value === ""){
-        balise.classList.add("error");
-    } else {
-        balise.classList.remove("error");
-    }
-};
-
-function verifierEmail(baliseMail) {
-    let emailRegExp = new RegExp("[a-z0-9._-]+@[a-z0-9._-]+\.[a-z0-9._-]+");
-    if (emailRegExp.test(baliseMail.value)) {
-        console.log("L'email est valide !");
-        baliseMail.classList.remove("error");
-    } else {
-        console.log("L'email est erroné !");
-        baliseMail.classList.add("error");
-    }
-};
-function verifierMdp(baliseMdp) {
-    let mdpRegExp = new RegExp("[a-z0-9._-]+$");
-    if (mdpRegExp.test(baliseMdp)) {
-        console.log("Le mot de passe est valide !");
-        baliseMail.classList.remove("error");
-    } else {
-        console.log("Le mot de passe est erroné !");
-        baliseMail.classList.add("error");
-    }
-}*/
-
-
-/*    try {
-        //verifLogin();        
-    }
-    catch (error) {
-        if (login.user !== mail.value) {
-            txtError.innerHTML = "Erreur dans l'email.";
-            console.log("Erreur de connexion avec l'email.");
-        }
-        if (login.password !== mdp.value) {
-            txtError.innerHTML = "Erreur dans le mot de passe.";
-            console.log("Erreur de connexion avec le mot de passe.");
-        }
-        if (login.user ==="", login.password ==="") {
-            txtError.innerHTML = "L'un des champs est vide.";
-            console.log("L'un des champs est vide.");
-        }
-    }*/
-
-    /*async function verifLogin () {
-    const login = {
-        user: event.target.querySelector("[id=email]").value,
-        password: event.target.querySelector("[id=mdp]").value,
-    };
-    
-    if (login.user === mail.value & login.password === mdp.value) {
-        //changement de page
-        window.location.href = "./index.html";
-    } else {
-        throw error;
-    }
-}*/
