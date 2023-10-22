f1f59571af31dae7a34415d79322daf085bd4beb
Audrey Luquet <audrey.luquet@tutanota.com>
2023-10-22 15:05:02 +0200

    ajout logout (effectif)

--- FrontEnd/assets/edit.js
+++ FrontEnd/assets/edit.js
@@ -1,11 +1,11 @@
 /********** IMPORT **********/
 import {filters} from "./script.js";
 
-
 /********** CONSTANTES **********/
 const online = localStorage.getItem("Token");
 export {online};
 
+const log = document.getElementById("log");
 const headband = document.querySelector(".headband");
 const edit = document.querySelector(".edit");
 
@@ -26,6 +26,11 @@ function hiddenFilters () {
     filters.setAttribute("aria-hidden", "true");
 }
 
+//ajout btn déco
+function logout () {
+    log.innerText = "logout";
+}
+
 
 /********** ECOUTEURS D'EVENEMENTS **********/
 //modif quand connecté
@@ -33,5 +38,13 @@ document.addEventListener("DOMContentLoaded", () => {
     if (online) {
         showEdition();
         hiddenFilters();
+        logout();
     }
 });
+
+log.addEventListener("click", () => {
+    //suppr token
+    localStorage.removeItem("Token", token);
+    //rediretion
+    window.location.replace("login.html");
+})
